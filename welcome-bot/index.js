4db45a9f2cfd0948338cc688d852f3779ed90391
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-03-13 12:12:35 +0530

    Read text from dir

--- welcome-bot/index.js
+++ welcome-bot/index.js
@@ -8,30 +8,31 @@ const port = process.env.PORT || 3000
 // Initialize the Web API Client
 const { WebClient } = require('@slack/client')
 const fs = require('fs')
-var welcomeMessage
-fs.readFile('./message.md', 'utf8', (err, data) => {
-  welcomeMessage = data
-  console.log(welcomeMessage)
-})
+
 const token = process.env.SLACK_TOKEN
 const slackWeb = new WebClient(token)
 // Setting Up Express to be used with the Events API
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-console.log(welcomeMessage)
+
 // Setting up this route to be used with the Slack Events middleware
 app.use('/slack/events', slackEvents.expressMiddleware())
 // Attach listeners to events by Slack Event "type".
 slackEvents.on('team_join', (event) => {
-  console.log(`Received a team_join event: user ${event.user.name} has joined.`)
-  console.log(welcomeMessage)
-  slackWeb.chat.postMessage({
-    channel: event.user.id,
-    text: welcomeMessage
-  }).then((res) => {
-    console.log(`Message sent: ${res.ts}`)
-  }).catch(console.error)
+  var welcomeMessage
+  fs.readFile('./welcome-bot/message.md', 'utf8', (err, data) => {
+    welcomeMessage = data
+    console.log('from data:',welcomeMessage)
+    console.log(`Received a team_join event: user ${event.user.name} has joined.`)
+    slackWeb.chat.postMessage({
+      channel: event.user.id,
+      text: welcomeMessage
+    }).then((res) => {
+      console.log(`Message sent: ${res.ts}`)
+    }).catch(console.error)
+    })
+  
 })
 
 // Handle errors (see `errorCodes` export)
