3648bb25fd3d8e6d2219801d223ad62aa8ed3ad6
andria-hibe <andriacohibe@gmail.com>
2020-07-24 12:53:21 +1200

    Refactor health-leaderboard cfn template

--- src/health-leaderboard/cloudformation/health-leaderboard-cfn-template.js
+++ src/health-leaderboard/cloudformation/health-leaderboard-cfn-template.js
@@ -1,22 +1,18 @@
 const cf = require('@mapbox/cloudfriend')
 
-const Resources = {
-  HealthLeaderboardLambda: {
-    Type: 'AWS::Lambda::Function',
-    Properties: {
-      FunctionName: 'health-leaderboard',
-      Handler: 'health-leaderboard-lambda.handler',
-      Role: cf.sub(
-        'arn:aws:iam::${AWS::AccountId}:role/service-role/test-slack-router-role-4hs4dpro'
-      ),
-      Code: {
-        S3Bucket: 'lambda-andria',
-        S3Key: 'health-leaderboard.zip',
-      },
-      Runtime: 'nodejs12.x',
-      Timeout: '30',
-    },
+const Parameters = {
+  BucketName: {
+    Type: 'String',
+    Description: 'Name of S3 bucket where Lambda code is saved',
+    Default: 'stork-us-east-1',
   },
+  GitSha: {
+    Type: 'String',
+    Description: 'Git SHA of latest commit for Lambda function',
+  },
+}
+
+const Resources = {
   HealthLeaderboardSNS: {
     Type: 'AWS::SNS::Topic',
     Properties: {
@@ -40,4 +36,20 @@ const Resources = {
   },
 }
 
-module.exports = { Resources }
+const lambda = new cf.shortcuts.Lambda({
+  LogicalName: 'HealthLeaderboardLambda',
+  Handler: 'src/health-leaderboard/health-leaderboard-lambda.handler',
+  Code: {
+    S3Bucket: cf.ref('BucketName'),
+    S3Key: cf.join('', ['bundles/slack-bots/', cf.ref('GitSha'), '.zip']),
+  },
+  Environment: {
+    Variables: {
+      AWS_ACCOUNT_ID: cf.accountId,
+    },
+  },
+  Runtime: 'nodejs12.x',
+  Timeout: '60',
+})
+
+module.exports = module.exports = cf.merge({ Parameters, Resources }, lambda)
