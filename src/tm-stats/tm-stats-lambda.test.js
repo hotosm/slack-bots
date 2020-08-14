d8aa209b92dbd620b682cdc2490380bd30f28cea
andria-hibe <andriacohibe@gmail.com>
2020-08-15 02:21:00 +1200

    Setup testing environment for tm-stats

---
+++ src/tm-stats/tm-stats-lambda.test.js
@@ -0,0 +1,38 @@
+const test = require('tape')
+const sinon = require('sinon')
+var AWS = require('@mapbox/mock-aws-sdk-js')
+const fetch = require('node-fetch')
+
+const lambda = require('./tm-stats-lambda')
+const utils = require('./slack-utils')
+
+const buildMockSNSEvent = (parameter) => {
+  return {
+    Records: [
+      {
+        EventSource: 'aws:sns',
+        EventVersion: '1.0',
+        EventSubscriptionArn:
+          'arn:aws:sns:us-east-1:xxxx:tm-stats:a7298a2b-faae-4621-b5ca-388762807cd8',
+        Sns: {
+          Type: 'Notification',
+          MessageId: '7de225df-xxxx-5336-a4cc-72458e7f0b54',
+          TopicArn: 'arn:aws:sns:us-east-1:xxxx:tm-stats',
+          Subject: 'SNS from Slack Slash Command',
+          Message: `{"token":"xxxx","team_id":"xxxx","team_domain":"hotosm","channel_id":"xxxx","channel_name":"xxxx","user_id":"xxxx","user_name":"xxxx","command":"%2Ftm-stats","text":"${parameter}","response_url":"https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT042TUWCB","trigger_id":"1267317526581.xxxx.dce29256095d10e5a4c261ed8f57b848"}`,
+          Timestamp: '2020-07-29T01:58:17.158Z',
+          SignatureVersion: '1',
+          Signature:
+            'dHCa+mefzs8ycz+xxxxUc5LGyo+gZqyVAx9ZGKBvPhQ9i8v5RX0ulcdodER1FvwrNWc7lHmoTgftTl/xyLkMPzgzECwkyjxnob5poIWhRgFUJSOI0BOLFoa37YEcT7SFiQpzzLP2N72g+bvY5Eub3JjgLRedny6V+YBrR654/fEoQY9+un4b9G798R1sEkO5uPEBRGX8TcuhQhd9LRe1+qDmaw4pnZqy9xvCKVll3wGIIWkjGcJWj4U0ynP3ri7k9rbabYX/LhcLyyjoRzQDx3uLFi8ba8Rr8xoobybWHM2ejThbnTO0oqTMv4B0CcPgjHJm5aUbvZ+7sf6Ug==',
+          SigningCertUrl:
+            'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-xxxx.pem',
+          UnsubscribeUrl:
+            'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:xxxx:tm-stats:xxxx-faae-4621-b5ca-xxxx',
+          MessageAttributes: {},
+        },
+      },
+    ],
+  }
+}
+
+test('description', async (t) => {})
