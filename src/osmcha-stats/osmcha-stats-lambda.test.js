9d6cbe1a0083cbe664ec8589bbf0151ff47d138b
andria-hibe <andriacohibe@gmail.com>
2020-08-14 19:53:50 +1200

    Test osmcha-stats error handling for exception thrown when querying projectID

--- src/osmcha-stats/osmcha-stats-lambda.test.js
+++ src/osmcha-stats/osmcha-stats-lambda.test.js
@@ -461,7 +461,78 @@ test('osmcha-stats return error message if user input invalid project ID', async
   sendToSlackStub.restore()
 })
 
-test.skip('osmcha-stats return success message if user input valid comment hashtag(s) and dataset is not too big', async (t) => {})
+test('osmcha-stats return generic error message if an exception was thrown - querying by project ID', async (t) => {
+  process.env.OSMCHA_BASE_URL = 'https://osmcha.org/'
+
+  const awsSSMStub = AWS.stub('SSM', 'getParameter', function () {
+    this.request.promise.returns(
+      Promise.resolve({ Parameter: { Value: 'faketokenfortesting' } })
+    )
+  })
+
+  const fetchStub = sinon.stub(fetch, 'Promise').returns(
+    Promise.resolve({
+      status: 200,
+      json: () => ({
+        aoiBBOX: [-77.952362061, 17.020414352, -77.566993713, 18.2509403230001],
+        projectInfo: {
+          locale: 'en',
+          name: 'COVID-19 - Saint Elizabeth, Jamaica',
+          shortDescription:
+            'The 2020 Atlantic Hurricane Season began June 1st. HOT has been requested by disaster preparedness and response actors to map buildings in Caribbean countries and other surrounding countries impacted by the hurricane season and the ongoing COVID-19 Pandemic.  Please join our global effort by mapping on this project.\n\nThe goal of this project is to digitize the buildings using **Maxar** imagery.',
+          description:
+            'The 2020 Atlantic Hurricane Season began June 1st. Disaster preparedness and response organizations in the Caribbean region are preparing to deal with the compounding impacts of hurricanes and the ongoing COVID-19 Pandemic. This will include needing to know how to balance resource distribution and shelter management with protocols for social distancing. \n\nHOT has been requested by disaster preparedness and response actors to map buildings in Caribbean countries and other surrounding countries impacted by the hurricane season. This data will assist these actors to have a complete buildings layer for their common operational datasets.\n\nGlobally HOT is committed to fighting [COVID-19](https://wiki.openstreetmap.org/wiki/COVID19) by providing 3 critical services:\n1. Helping government agencies and responders with basic data needs: we’re providing this through the  [UN’s Humanitarian Data Exchange](https://data.humdata.org/), among other ways.\n2. Helping to Identify populations living in places most at risk: prioritizing our existing queue of mapping projects to get volunteers immediately mapping areas with high proportions of COVID-19 cases, or of greater vulnerability.\n3. Creating new mapping projects in highest-risk places; which is what this project does. Every feature you map will help in this objective!\n\nThe goal of this project is to digitize the buildings using **Maxar Imagery**. While other services may have sharper imagery, Maxar imagery for this area is the latest imagery available for digitization. \n\nMany areas of the Caribbean have been previously mapped during disaster activations and other projects. We need to continuously work to update and improve the map - especially as new imagery becomes available. Please help us complete and update the base map by looking for missing or new buildings in areas where OSM mapping has already occurred. \n',
+          instructions:
+            'Project Specific Mapping Notes\n=========================\n* **Imagery:** Please use **Maxar Imagery**. You may switch to Bing, ESRI or others for comparison but Maxar has been chosen for this area due to resolution and clarity. \n* Many of these squares will already be partially or fully mapped, map in more if needed or fix up the existing mapping, or just mark it "Completely Mapped" if it is already complete.\n* **Existing mapping does not match imagery** - This happens in some areas, when different imagery sources are used. If this occurs, check to make sure you are using Bing imagery. If necessary, adjust existing mapping to align with Bing, then continue mapping. \n* **Alternative imagery is better** - Sometimes due to shadows, alignment and other factors it may be easier to trace features using other imagery. That is fine as long as you adjust for any offset. When finished please leave a comment in the Tasking Manager when you mark done/stop mapping to say you used alternative imagery.  See [LearnOSM](https://learnosm.org/en/josm/correcting-imagery-offset/) for detailed instructions on handling imagery offset, or [Aerial Imagery & Alignment](https://learnosm.org/en/hot-tips/imagery/) for adjusting in the iD editor.\n\nBuildings\n=======\n* Two-minute Tutorial: Buildings - https://www.youtube.com/watch?v=E1YJV6I_rhY \n<iframe width="560" height="315" src="https://www.youtube.com/embed/E1YJV6I_rhY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>\n* Please accurately outline all the buildings you can find. The outline should be for the full size of the building even if it is partly covered by trees in the imagery.\n* Take care to not include the building shadow in the building outline.\n* After drawing the outline and tagging as a building, **use the \'Q\' key in the iD & JOSM editors to "square" the corners**. We suggest using the Buildings plug-in in JOSM to make building tracing more efficient. \n* Buildings may be very close, but do not actually touch each other, try to map them close to each other without letting them connect or share nodes with each other, roads or residential area outlines. In the iD web editor, holding down the "alt" key will keep nodes from "snapping" to each other and accidentally connecting.\n* In the iD web editor, you will only have the option to tag new features as buildings, see below for editing or adding other features.\n\n**Only if you have personal knowledge of a building**, please add that information to the building, like the name or type of building (hospital, school, gas station, etc)\n',
+          perTaskInstructions: '',
+        },
+        changesetComment: '#hotosm-project-8989   #covid19 #COVJam',
+        created: '2020-06-26T13:30:45.790345Z',
+      }),
+    })
+  )
+
+  const fetchChangesetDataStub = sinon
+    .stub(lambda, 'fetchChangesetData')
+    .rejects(new Error('Expected error'))
+
+  const sendToSlackStub = sinon
+    .stub(utils, 'sendToSlack')
+    .returns(Promise.resolve(null))
+
+  await lambda.handler(buildMockSNSEvent(8989))
+
+  sinon.assert.callCount(awsSSMStub, 1)
+  sinon.assert.callCount(fetchStub, 1)
+  sinon.assert.callCount(fetchChangesetDataStub, 1)
+  sinon.assert.callCount(sendToSlackStub, 1)
+
+  t.equal(
+    utils.sendToSlack.calledWith('https://hooks.slack.com/commands/T042TUWCB', {
+      response_type: 'ephemeral',
+      text:
+        ':x: Something went wrong with your request. Please try again and if the error persists, post a message at <#C319P09PB>.', // move to Parameter Store so it can be used for all generic errors?
+    }),
+    true
+  )
+
+  t.end()
+  process.env.OSMCHA_BASE_URL = undefined
+  awsSSMStub.restore()
+  fetchStub.restore()
+  fetchChangesetDataStub.restore()
+  sendToSlackStub.restore()
+})
+
+test.skip('osmcha-stats return correct message if user input valid comment hashtag(s) and dataset is not too big', async (t) => {
+  process.env.OSMCHA_BASE_URL = 'https://osmcha.org/'
+
+  const awsSSMStub = AWS.stub('SSM', 'getParameter', function () {
+    this.request.promise.returns(
+      Promise.resolve({ Parameter: { Value: 'faketokenfortesting' } })
+    )
+  })
+})
 
 test.skip('osmcha-stats return error message if user input comment hashtag(s) with zero results', async (t) => {})
 
