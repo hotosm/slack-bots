3eee017ab1a98dd0ba05f10a2895a826b2f3af02
andria-hibe <andriacohibe@gmail.com>
2020-08-15 17:54:41 +1200

    Test that sendProjectStats(tm-stats) returns success block in successful query

--- src/tm-stats/tm-stats-lambda.test.js
+++ src/tm-stats/tm-stats-lambda.test.js
@@ -6,6 +6,7 @@ const fetch = require('node-fetch')
 const lambda = require('./tm-stats-lambda')
 const utils = require('./slack-utils')
 const sendTaskingManagerStats = require('./send-tm-stats')
+const sendProjectStats = require('./send-project-stats')
 
 const buildMockSNSEvent = (parameter) => {
   return {
@@ -207,7 +208,97 @@ test('sendTmStats returns error if JSON parsing failed', async (t) => {
   sendToSlackStub.restore()
 })
 
-test.skip('sendProjectStats returns success block in successful query', async (t) => {})
+test('sendProjectStats returns success block in successful query', async (t) => {
+  const fetchStub = sinon.stub(fetch, 'Promise')
+
+  fetchStub.onCall(0).returns(
+    Promise.resolve({
+      status: 200,
+      json: () => ({
+        projectInfo: {
+          locale: 'en',
+          name: 'COVID-19 - Saint Elizabeth, Jamaica',
+          shortDescription:
+            'The 2020 Atlantic Hurricane Season began June 1st. HOT has been requested by disaster preparedness and response actors to map buildings in Caribbean countries and other surrounding countries impacted by the hurricane season and the ongoing COVID-19 Pandemic.',
+          description:
+            'The 2020 Atlantic Hurricane Season began June 1st. Disaster preparedness and response organizations in the Caribbean region are preparing to deal with the compounding impacts of hurricanes and the ongoing COVID-19 Pandemic. This will include needing to know how to balance resource distribution and shelter management with protocols for social distancing. \n\nHOT has been requested by disaster preparedness and response actors to map buildings in Caribbean countries and other surrounding countries impacted by the hurricane season. This data will assist these actors to have a complete buildings layer for their common operational datasets.\n\nGlobally HOT is committed to fighting [COVID-19](https://wiki.openstreetmap.org/wiki/COVID19) by providing 3 critical services:\n1. Helping government agencies and responders with basic data needs: we’re providing this through the  [UN’s Humanitarian Data Exchange](https://data.humdata.org/), among other ways.\n2. Helping to Identify populations living in places most at risk: prioritizing our existing queue of mapping projects to get volunteers immediately mapping areas with high proportions of COVID-19 cases, or of greater vulnerability.\n3. Creating new mapping projects in highest-risk places; which is what this project does. Every feature you map will help in this objective!\n\nThe goal of this project is to digitize the buildings using **Maxar Imagery**. While other services may have sharper imagery, Maxar imagery for this area is the latest imagery available for digitization. \n\nMany areas of the Caribbean have been previously mapped during disaster activations and other projects. We need to continuously work to update and improve the map - especially as new imagery becomes available. Please help us complete and update the base map by looking for missing or new buildings in areas where OSM mapping has already occurred. \n',
+        },
+        percentMapped: 100,
+        percentValidated: 44,
+        status: 'PUBLISHED',
+      }),
+    })
+  )
+  fetchStub.onCall(1).returns(
+    Promise.resolve({
+      status: 200,
+      json: () => ({
+        'projectArea(in sq.km)': 1200.456,
+        totalMappers: 150,
+        totalTasks: 1247,
+      }),
+    })
+  )
+
+  const sendToSlackStub = sinon
+    .stub(utils, 'sendToSlack')
+    .returns(Promise.resolve(null))
+
+  await sendProjectStats('responseURL', 'tmApiBaseUrl', 'tmBaseUrl/', 8989)
+
+  sinon.assert.callCount(fetchStub, 2)
+  sinon.assert.callCount(sendToSlackStub, 1)
+
+  t.equal(
+    utils.sendToSlack.calledWith('responseURL', {
+      response_type: 'ephemeral',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text:
+              '<tmBaseUrl/project/8989|*8989 - COVID-19 - Saint Elizabeth, Jamaica*>',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text:
+              '*PUBLISHED* - The 2020 Atlantic Hurricane Season began June 1st. HOT has been requested by disaster preparedness and response actors to map buildings in Caribbean countries and other surrounding countries impacted by the hurricane season and the ongoing COVID-19 Pandemic.',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: ':world_map: *Project Area* - 1200.456} sq. km.',
+          },
+        },
+        { type: 'divider' },
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text:
+                ':female-construction-worker::male-construction-worker: *Number of Contributors*: 150',
+            },
+            { type: 'mrkdwn', text: ':clipboard: *Number of Tasks*: 1247' },
+            { type: 'mrkdwn', text: ':round_pushpin: *100%* Mapped' },
+            { type: 'mrkdwn', text: ':white_check_mark: *44%* Validated' },
+          ],
+        },
+      ],
+    }),
+    true
+  )
+
+  t.end()
+  fetchStub.restore()
+  sendToSlackStub.restore()
+})
 
 test.skip('sendProjectStats returns error if fetch status is not 200', async (t) => {})
 
