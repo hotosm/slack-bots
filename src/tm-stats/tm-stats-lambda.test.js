dd5ae010e9d750521401c64678d8a137bc7b7cc3
andria-hibe <andriacohibe@gmail.com>
2020-08-15 16:51:46 +1200

    Test that sendTmStats(tm-stats) returns error if fetch status is not 200

--- src/tm-stats/tm-stats-lambda.test.js
+++ src/tm-stats/tm-stats-lambda.test.js
@@ -144,7 +144,35 @@ test('sendTmStats returns success block in successful query', async (t) => {
   sendToSlackStub.restore()
 })
 
-test.skip('sendTmStats returns error if fetch status is not 200', async (t) => {})
+test('sendTmStats returns error if fetch status is not 200', async (t) => {
+  const fetchStub = sinon.stub(fetch, 'Promise').returns(
+    Promise.resolve({
+      status: 500,
+    })
+  )
+
+  const sendToSlackStub = sinon
+    .stub(utils, 'sendToSlack')
+    .returns(Promise.resolve(null))
+
+  await sendTaskingManagerStats('responseURL', 'tmApiBaseUrl')
+
+  sinon.assert.callCount(fetchStub, 1)
+  sinon.assert.callCount(sendToSlackStub, 1)
+
+  t.equal(
+    utils.sendToSlack.calledWith('responseURL', {
+      response_type: 'ephemeral',
+      text:
+        ':x: Something went wrong with your request. Please try again and if the error persists, post a message at <#C319P09PB>',
+    }),
+    true
+  )
+
+  t.end()
+  fetchStub.restore()
+  sendToSlackStub.restore()
+})
 
 test.skip('sendTmStats returns error if JSON parsing failed', async (t) => {})
 
